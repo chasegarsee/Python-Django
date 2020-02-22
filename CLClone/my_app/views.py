9acdb644cb8b83970d74f350426f000f40906879
chasegarsee <chasegarsee@gmail.com>
2020-02-22 13:50:35 -0600

    search functionality complete

--- CLClone/my_app/views.py
+++ CLClone/my_app/views.py
@@ -1,8 +1,12 @@
 import requests
+from requests.compat import quote_plus
 from django.shortcuts import render
 from bs4 import BeautifulSoup
+from . import models
 
 # Create your views here.
+
+CRAIGS_BASIC_URL = 'https://nashville.craigslist.org/search/?query={}'
 
 
 def home(request):
@@ -11,7 +15,12 @@ def home(request):
 
 def new_search(request):
     search = request.POST.get('search')
-    print(search)
+    models.Search.objects.create(search=search)
+    final_url = CRAIGS_BASIC_URL.format(quote_plus(search))
+   # print(quote_plus(final_url))
+    res = requests.get(final_url)
+    data = res.text
+    print(data)
     frontend_data = {
         "search": search,
     }
