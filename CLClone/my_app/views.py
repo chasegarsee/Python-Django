d7db0fa15091927a79737759d41ca1bb014ee869
chasegarsee <chasegarsee@gmail.com>
2020-02-22 14:33:44 -0600

    getting title price and link to postings

--- CLClone/my_app/views.py
+++ CLClone/my_app/views.py
@@ -20,8 +20,25 @@ def new_search(request):
    # print(quote_plus(final_url))
     res = requests.get(final_url)
     data = res.text
-    print(data)
+    soup = BeautifulSoup(data, features="html.parser")
+
+    post_listings = soup.find_all("li", {"class": 'result-row'})
+
+    final_postings = []
+
+    for post in post_listings:
+        post_title = post.find(class_="result-title").text
+        post_url = post.find("a").get("href")
+
+        if post.find(class_='result-price'):
+            post_price = post.find(class_='result-price').text
+        else:
+            post_price = "N/A"
+
+        final_postings.append((post_title, post_url, post_price))
+
     frontend_data = {
         "search": search,
+        "final_postings": final_postings
     }
     return render(request, "my_app/new_search.html", frontend_data)
